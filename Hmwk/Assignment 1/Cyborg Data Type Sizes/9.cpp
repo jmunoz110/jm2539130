ffeb2895ad66dacf739e35ff9d2a047ffa2bfb78
Jonathan Munoz <jmunoz110@student.rcc.edu>
2016-01-07 11:14:27 -0800

    Signed-off-by: Jonathan Munoz <jmunoz110@student.rcc.edu>

---
+++ Hmwk/Assignment 1/Cyborg Data Type Sizes/9.cpp
@@ -0,0 +1,15 @@
+// Jonathan Munoz
+// Cyborg Data Type Sizes
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The char data type uses " << sizeof(char) << " byte." << endl;
+    cout << "The int data type uses " << sizeof(int) << " bytes." << endl;
+    cout << "The float data type uses " << sizeof(float) << " bytes" << endl;
+    cout << "The double data type uses " << sizeof(double) << " bytes" << endl;
+    
+    return 0;
+}
