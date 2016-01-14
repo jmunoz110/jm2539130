63c6b42490ed51695aeacdd75ee50dfb0b81edae
Jonathan Munoz <jmunoz110@student.rcc.edu>
2016-01-14 10:24:00 -0800

    Signed-off-by: Jonathan Munoz <jmunoz110@student.rcc.edu>

--- Hmwk/Assignment 2/10. Celsius to Fahrenheit/main.cpp
+++ Hmwk/Assignment 2/10. Celsius to Fahrenheit/main.cpp
@@ -6,16 +6,16 @@
 using namespace std;
 int main()
 {
-	float Celsius, Fahrenheit;
+	float Cels, Fahr;
 	
 	cout << "This program converts Celsius temperatures to Fahrenheit temperatures." << endl;
 	cout << "Enter a Celsius temperature: ";
-	cin >> Celsius;
+	cin >> Cels;
 	
-	Fahrenheit = 9.0 / 5 * Celsius + 32;
+	Fahr = 9.0 / 5 * Cels + 32;
 	
-	cout << fixed << setprecision(1) << endl << Celsius << " " << static_cast<char>(248);
-	cout << "C is equivalent to " << Fahrenheit << " " << static_cast<char>(248) << "F." << endl;
+	cout << fixed << setprecision(1) << endl << Cels << " " << static_cast<char>(248);
+	cout << "C is equivalent to " << Fahr << " " << static_cast<char>(248) << "F." << endl;
 	
 	return 0;
 }
