b7351523b05b6346f7ff5d1122ba6bbe912d4c42
Jonathan Munoz <jmunoz110@student.rcc.edu>
2016-01-20 07:58:21 -0800

    Signed-off-by: Jonathan Munoz <jmunoz110@student.rcc.edu>

--- Hmwk/Hello World/main.cpp
+++ Hmwk/Hello World/main.cpp
@@ -1,8 +1,13 @@
+// Preprocessor Directive
 #include <iostream>
 using namespace std;
 
+// Program
 int main()
 {
+    // Display sentence on monitor
     cout << "Hello, World!";
+    
+    // End the program
     return 0;
 }
